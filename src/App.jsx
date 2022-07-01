546c27f24b089e4f224e5fbaa1c1e594e493a812
Evgen <nazarenkoevgen99@gmail.com>
2022-07-01 21:32:45 +0300

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import styles from "./App.module.css";
 
 class App extends Component {
-    render() { 
-        return (
-            <>
-                <h1 className={styles.mainTitle}>Hello, user! Make your first finance wheel!</h1>
-            </>
-        );
-    }
+  render() {
+    return (
+      <>
+        <h1 className={styles.mainTitle}>
+          Hello, user! Make your first finance wheel!
+        </h1>
+      </>
+    );
+  }
 }
- 
+
 export default App;
